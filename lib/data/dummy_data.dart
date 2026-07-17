a22bb894c8caab1636d9d9b5714344e78f5f0484
Chocolate_Starfish <alex.beigel@gmail.com>
2026-07-17 15:29:08 +0300

    feat(dummy_data): added a list of categories with dummy data to start working with on UI

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'package:calorio/models/category.dart';
+
+const availableCategories = [
+  Category(
+    id: 'c1',
+    title: 'italian',
+    color: Colors.white,
+  ),
+  Category(
+    id: 'c2',
+    title: 'french',
+    color: Colors.blueGrey,
+  ),
+  Category(
+    id: 'c3',
+    title: 'spanish',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c4',
+    title: 'greek',
+    color: Colors.lightBlue,
+  ),
+  Category(
+    id: 'c5',
+    title: 'mexican',
+    color: Colors.orange,
+  ),
+  Category(
+    id: 'c6',
+    title: 'tex-mex',
+    color: Colors.orange, // Matches Mexican
+  ),
+  Category(
+    id: 'c7',
+    title: 'japanese',
+    color: Colors.teal,
+  ),
+  Category(
+    id: 'c8',
+    title: 'chinese',
+    color: Colors.teal, // Matches Japanese
+  ),
+  Category(
+    id: 'c9',
+    title: 'korean',
+    color: Colors.teal, // Matches Japanese & Chinese
+  ),
+  Category(
+    id: 'c10',
+    title: 'thai',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c11',
+    title: 'vietnamese',
+    color: Colors.green, // Matches Thai
+  ),
+  Category(
+    id: 'c12',
+    title: 'indian',
+    color: Colors.deepOrange,
+  ),
+  Category(
+    id: 'c13',
+    title: 'lebanese',
+    color: Colors.brown,
+  ),
+  Category(
+    id: 'c14',
+    title: 'turkish',
+    color: Colors.brown, // Matches Lebanese
+  ),
+  Category(
+    id: 'c15',
+    title: 'moroccan',
+    color: Colors.brown, // Matches Lebanese & Turkish
+  ),
+  Category(
+    id: 'c16',
+    title: 'american',
+    color: Colors.indigo,
+  ),
+  Category(
+    id: 'c17',
+    title: 'british',
+    color: Colors.indigo, // Matches American
+  ),
+  Category(
+    id: 'c18',
+    title: 'caribbean',
+    color: Colors.cyan,
+  ),
+  Category(
+    id: 'c19',
+    title: 'brazilian',
+    color: Colors.cyan, // Matches Caribbean
+  ),
+  Category(
+    id: 'c20',
+    title: 'vegetarian',
+    color: Colors.lightGreen,
+  ),
+  Category(
+    id: 'c21',
+    title: 'vegan',
+    color: Colors.lightGreen, // Matches Vegetarian
+  ),
+  Category(
+    id: 'c22',
+    title: 'seafood',
+    color: Colors.blue,
+  ),
+  Category(
+    id: 'c23',
+    title: 'bbq & grill',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c24',
+    title: 'breakfast & brunch',
+    color: Colors.yellow,
+  ),
+  Category(
+    id: 'c25',
+    title: 'desserts',
+    color: Colors.pink,
+  ),
+  Category(
+    id: 'c26',
+    title: 'baking',
+    color: Colors.pink, // Matches Desserts
+  ),
+];
