bbf9e3e4bebad876302eb375b2d6a64d394e05c8
Chocolate_Starfish <alex.beigel@gmail.com>
2026-07-17 15:31:32 +0300

    refactor(categories_screen): removed dummy Text widgets, and added a loop that outputs every category from the dummy_data to a grid item

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,3 +1,5 @@
+import 'package:calorio/data/dummy_data.dart';
+import 'package:calorio/widgets/category_grid_item.dart';
 import 'package:flutter/material.dart';
 
 class CategoriesScreen extends StatelessWidget {
@@ -17,13 +19,9 @@ class CategoriesScreen extends StatelessWidget {
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
-        children: const [
-          Text('1', style: TextStyle(color: Colors.white)),
-          Text('2', style: TextStyle(color: Colors.white)),
-          Text('3', style: TextStyle(color: Colors.white)),
-          Text('4', style: TextStyle(color: Colors.white)),
-          Text('5', style: TextStyle(color: Colors.white)),
-          Text('6', style: TextStyle(color: Colors.white)),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(category: category),
         ],
       ),
     );
