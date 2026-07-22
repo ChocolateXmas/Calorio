c9b296b0bfa95331c3f1508cb6bfd0495fbf256c
Chocolate_Starfish <alex.beigel@gmail.com>
2026-07-22 20:52:18 +0300

    feat(`MealDetailsScreen`) added ingredients & steps info widget, and wrapped all inside a `SingleChildScrollView` on enable scrolling

--- lib/widgets/meal_widgets/meal_details.dart
+++ lib/widgets/meal_widgets/meal_details.dart
@@ -6,18 +6,78 @@ class MealDetailsScreen extends StatelessWidget {
 
   final Meal meal;
 
+  Container _divider(BuildContext context) => Container(
+    height: 1,
+    width: MediaQuery.of(context).size.width * 0.7,
+    color: Colors.white60,
+    margin: EdgeInsets.symmetric(vertical: 24),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Center(
-        child: Image.network(
-          meal.imageURL,
-          height: 300,
-          width: double.infinity,
-          fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(
+              meal.imageURL,
+              height: 300,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 16),
+            Container(
+              margin: EdgeInsets.only(bottom: 12),
+              child: Text(
+                'Ingredients',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            for (final ingredient in meal.ingredients)
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 5,
+                ),
+                child: Text(
+                  ingredient,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurface,
+                  ),
+                ),
+              ),
+            _divider(context),
+            Container(
+              margin: EdgeInsets.only(bottom: 12),
+              child: Text(
+                'Steps',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            for (final (i, step) in meal.steps.indexed)
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 10,
+                ),
+                child: Text(
+                  '${i + 1}. $step',
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurface,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+          ],
         ),
       ),
     );
